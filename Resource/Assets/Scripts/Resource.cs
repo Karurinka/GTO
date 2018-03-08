e2a2381d379e6d724fad39365fd21ac903283d5a
Karurinka <michellebroens96@gmail.com>
2018-03-08 10:51:49 +0100

    Added Resource project

---
+++ Resource/Assets/Scripts/Resource.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Resource : MonoBehaviour {
+
+    public string Name;
+    public int Amount;
+    public int StartingAmount;
+    public UnityEvent OnValueChanged;
+
+    private void Awake()
+    {
+        Amount = StartingAmount;
+    }
+
+    public void AddAmount(int amount)
+    {
+        if (amount < 0)
+        {
+            Debug.LogError("Amount can't be smaller than 0");
+        }
+        else
+        {
+            Amount += amount;
+        }
+    }
+
+    public void RemoveAmount(int amount)
+    {
+        if (amount < 0)
+        {
+            Debug.LogError("Amount can't be smaller than 0");
+        }
+        else
+        {
+            Amount -= amount;
+        }
+    }
+
+    public void UpdateUI()
+    {
+        //call the self made event in unity
+        OnValueChanged.Invoke();
+    }
+}
---
+++ Resource/Assets/Scripts/ResourceUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Resource))]
+public class ResourceUI : MonoBehaviour {
+
+    public Text Value;
+    public Text Label;
+
+    private Resource resource;
+
+    private void Awake()
+    {
+        resource = GetComponent<Resource>();
+        //adds listener to when onvaluechanged is activated
+        resource.OnValueChanged.AddListener(UpdateUI);
+    }
+
+    private void Start()
+    {
+        Label.text = resource.Name;
+        Value.text = resource.Amount.ToString();
+    }
+
+    public void UpdateUI()
+    {
+        Value.text = resource.Amount.ToString();
+    }
+}
